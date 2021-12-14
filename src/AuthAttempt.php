aeff16b13f4319e990cd972dbd43f354e2f20ec9
Erick Zarate Hernandez <zaraterick@outlook.com>
2021-12-13 18:08:21 -0600

    Add AuthAttempt

---
+++ src/AuthAttempt.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ZarateSystems\LaravelCognitoAuth;
+
+class AuthAttempt
+{
+    /**
+     * @var bool
+     */
+    protected bool $successful;
+
+    /**
+     * @var array
+     */
+    protected array $response = [];
+
+    /**
+     * AuthAttempt constructor.
+     *
+     * @param bool $successful
+     * @param array $response
+     */
+    public function __construct(bool $successful, array $response = [])
+    {
+        $this->successful = $successful;
+        $this->response = $response;
+    }
+
+    /**
+     * Was the authentication attempt successful.
+     *
+     * @return bool
+     */
+    public function successful(): bool
+    {
+        return $this->successful;
+    }
+
+    /**
+     * Get the response data from an unsuccessful authentication attempt.
+     *
+     * @return array|null
+     */
+    public function getResponse(): ?array
+    {
+        return $this->response;
+    }
+}
